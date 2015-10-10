97650d7953838918c327096f82666431ce93c6a2
Pedro Costa <pedro.vendascosta@gmail.com>
2015-10-10 20:49:41 +0100

    Solved issue with read_dataset when limit=-1

--- read_dataset.py
+++ read_dataset.py
@@ -2,14 +2,18 @@ import csv
 import random
 import numpy as np
 
-#TODO limit can't be -1, or else we have "sample size larger than population errors"... at least on my machine.
+def split(X,y,n):
+    if n > 0:
+        rows_to_include = random.sample(range(len(X)), n)
+        X = [X[row] for row in rows_to_include]
+        y = [y[row] for row in rows_to_include]
+
+    return X,y
+
 def read_train(file_name='train.csv', limit=-1):
     X = []
     # Train data has a label
     y = []
-    #TODO I don't understand this line, but limit can't be -1
-    rows_to_include = random.sample(range(784), limit)
-    i = 0
     with open(file_name,'rt') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         # Skip header
@@ -17,22 +21,15 @@ def read_train(file_name='train.csv', limit=-1):
         # row = 'label pixel0 pixel1 pixel2 pixel3 ... pixel783'
         # label in [0...9]
         for row in reader:
-            if i in rows_to_include:
-                r = [int(elem) for elem in row]
-                y.append(r[0])
-                X.append(np.array(r[1:len(r)]))
+            r = [int(elem) for elem in row]
+            y.append(r[0])
+            X.append(np.array(r[1:len(r)]))
 
-            i = i + 1
-    return X,y
+    return split(X,y,limit)
 
-
-    #TODO limit can't be -1, or else we have "sample size larger than population errors"... at least on my machine.
 def read_test(file_name='test.csv', limit=-1):
     X = []
     #But Test data has no label.
-    #TODO I don't understand this line, but limit can't be -1
-    rows_to_include = random.sample(range(784), limit)
-    i = 0
     with open(file_name,'rt') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         # Skip header
@@ -40,9 +37,18 @@ def read_test(file_name='test.csv', limit=-1):
         # row = 'label pixel0 pixel1 pixel2 pixel3 ... pixel783'
         # label in [0...9]
         for row in reader:
-            if i in rows_to_include:
-                r = [int(elem) for elem in row]
-                X.append(r)
+            r = [int(elem) for elem in row]
+            X.append(r)
 
-            i = i + 1
+    y = [i for i in range(len(X))]
+    X,_ = split(X,y,limit)
     return X
+
+if __name__ == '__main__':
+
+    X,y = read_train(limit=-1)
+    print(len(X))
+    print(len(y))
+
+    X = read_test(limit=-1)
+    print(len(X))
